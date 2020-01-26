21bbfbe763af6a3ef7f1c2efd95c88b716ed0e39
James Wilcox <50497892+jamesNWT@users.noreply.github.com>
2020-01-26 02:04:12 -0400

    we now get a random image from firebase

--- js/setBackground.js
+++ js/setBackground.js
@@ -4,16 +4,47 @@ var storage = firebase.storage();
 // Create a storage reference from our storage service
 var storageRef = storage.ref();
 
-var doryRef = storageRef.child('backgrounds/dory.jpg');
+var folderRef = storageRef.child('backgrounds');
 
-doryRef.getDownloadURL().then(function(url) {
+var images = new Array();
 
-    var background = document.createElement('style');
-    background.innerHTML = `
+// Find all the prefixes and items.
+folderRef.listAll().then(function (res) {
+
+    res.items.forEach(function (itemRef) {
+        images.push(itemRef.fullPath)
+    });
+
+    setImage(images.length)
+
+
+
+}).catch(function (error) {
+    // Uh-oh, an error occurred!
+});
+
+function setImage(length) {
+
+    var randomNumber = Math.floor(Math.random() * images.length);
+
+    console.log(images);
+
+    console.log(randomNumber);
+
+    var imageRef = storageRef.child(images[randomNumber]);
+
+    console.log(imageRef);
+
+    imageRef.getDownloadURL().then(function (url) {
+
+        var background = document.createElement('style');
+        background.innerHTML = `
     body {
         background-image: url(${url});
     }
     `;
 
-    document.head.appendChild(background);
-})
+        document.head.appendChild(background);
+    })
+}
+
