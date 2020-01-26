47247b41e90c625b7aa82a47052f0c02d309bc5e
James Wilcox <50497892+jamesNWT@users.noreply.github.com>
2020-01-26 01:08:41 -0400

    background image now comes from firebase. also added to gitignore

---
+++ js/setBackground.js
@@ -0,0 +1,21 @@
+// Get a reference to the storage service, which is used to create references in your storage bucket
+var storage = firebase.storage();
+
+// Create a storage reference from our storage service
+var storageRef = storage.ref();
+
+var doryRef = storageRef.child('backgrounds/dory.jpg');
+
+doryRef.getDownloadURL().then(function(url) {
+
+    var img = document.getElementById("sample");
+
+    var background = document.createElement('style');
+    background.innerHTML = `
+    body {
+        background-image: url(${url});
+    }
+    `;
+
+    document.head.appendChild(background);
+})
